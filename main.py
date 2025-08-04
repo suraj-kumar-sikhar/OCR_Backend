41b7c95cfac81e013db880ee6a2fcdf2ced23576
suraj-kumar-sikhar <surajkumarsikhar08@gmail.com>
2025-08-04 17:26:00 +0530

    /

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ocr-frontend-tau.vercel.app/"],       
+    allow_origins=["https://ocr-frontend-tau.vercel.app"],       
     allow_credentials=True,
     allow_methods=["*"],             
     allow_headers=["*"],  
